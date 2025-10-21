66921e831649f60824a70b76c410d0ff7ae959fb
Harshavardhan Deshmukh <harshaal.ad@gmail.com>
2025-10-21 11:05:06 +0530

    Feature:Added new DocType Device Data Queue..

---
+++ beetwin_iot/beetwin_iot/doctype/device_data_queue/test_device_data_queue.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Logicare Systems Private Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDeviceDataQueue(FrappeTestCase):
+	pass
